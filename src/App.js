6e0a00a0ddff8d9db55f3ee969bc9938d2f75963
Gustavo La Cruz <gscruz627@protonmail.com>
2023-04-10 22:14:17 -0500

    new attempt to push at ghpages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
@@ -21,7 +21,7 @@ function App() {
   const finalTheme = useMemo( () => createTheme(themeSettings(theme)), [theme]);
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <ThemeProvider theme={finalTheme}>
           <CssBaseline>
             <Routes>
@@ -39,7 +39,7 @@ function App() {
             </Routes>
           </CssBaseline>
         </ThemeProvider>
-     </BrowserRouter>
+     </HashRouter>
         
     </div>
   )
